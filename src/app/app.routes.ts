b6ccedc7b94f8d5c81a6aea00e74d07fc88c750e
sydny quinquino <sjquinquino71936@liceo.edu.ph>
2026-02-03 10:13:03 +0800

    Midterm Activity

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  { path: 'posts', component: PostsComponent }
+];
